8c7fa71993a6638d786abaac175d33687eb44609
Andrey9911 <chivirevandrey775@gmail.com>
2021-11-16 18:54:39 +0300

    moving

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,9 +2,12 @@ const app = new Vue({
     el: '#app',
     data:{
         age: 'age',
-        weight: 'weight'
+        weight: 'weight',
+        bb: '1px solid #8a2be2',
+        //active: true
     },
     methods:{
+        
         link(par){
             console.log(par + '/' + par + '.html')
             document.location.href = '../' + par + '/' + par + '.html'
@@ -17,6 +20,10 @@ const app = new Vue({
                 e.path[1].lastChild.style.animation = '0.3s linehover ease-in'
             })
             e.path[1].lastChild.style.animation = ''
+        },
+        str(e){
+            console.log(e)
+            document.location.href = '../main/index.html'
         }
     }
 })
