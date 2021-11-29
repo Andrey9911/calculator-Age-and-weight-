eaf211024a466cbb0985cd6f89e5dd409ec77205
Andrey9911 <chivirevandrey775@gmail.com>
2021-11-29 17:02:21 +0300

    finished

--- javaScript/main.js
+++ javaScript/main.js
@@ -12,8 +12,14 @@ const app = new Vue({
         isActive: false,
         isDrag: 'none',
         not: true,
-        rez: 0,
+        rezult: {
+            rez: 0,
+            rez2: 0,
+            rez3: 0
+        },
         z: 0,
+        year_bir: 0,
+        isDate: true,
         
     },
     methods:{
@@ -57,17 +63,26 @@ const app = new Vue({
                     this.isActive = true
                     return this.warning = this.h + ' не число'}
                else{
+                
                     this.not = true
                     this.isActive = false
                     this.warning = ''
                     this.rez = this.h - this.w
                     rez = this.rez
                     console.log(rez)
-                    if(rez < 85 || rez > 120) this.z = 3
+                    if(rez > 150){
+                        this.not = false
+                        this.isActive = true
+
+                        this.warning = 'некоректные данные'
+                    }
+                    else if(rez < 85 || rez > 120) this.z = 3
 
                     else if(rez < 90 || rez > 110) this.z = 2
 
-                    else this.z = 1
+                    else if(rez > 90 || rez < 110) this.z = 1
+
+                    
                 }
                 // console.log(typeof(h))
                 
@@ -94,6 +109,65 @@ const app = new Vue({
             notDrag(e){
                 this.isDrag = 'none'
                 // e.target.style.opasity = this.isDrag + 'px'
+            },
+            goDate(e){
+                let year = new Date()
+                let rez_next
+                year_bir = new Date(this.year_bir)
+                this.rezult.rez = year.getFullYear() - year_bir.getFullYear()
+                let yearN = new Date(year_bir.setFullYear(year.getFullYear() + 1))
+                
+                
+                // console.log(year_bir.getFullYear())
+                //console.log(nextYear)
+
+                rez_next = Math.abs(yearN.getTime() - year.getTime()) 
+                rez_next =  Math.ceil(rez_next / (1000 * 3600 * 24))
+                this.rezult.rez2 = rez_next
+                this.rezult.rez3 = 365  - rez_next
+                //this.rezult.rez2 = 
+                // console.log(year_bir.getFullYear())
+                // console.log(year.getFullYear())
+
+               
+                // console.log(yearN.getFullYear())
+                // console.log(Boolean(year_bir.setFullYear(year.getFullYear) <= year.getFullYear()))
+
+                year_bir2 = new Date(this.year_bir)
+
+                // console.log(year_bir2.getFullYear())
+
+                
+
+                
+                console.log(Boolean((year_bir2.getFullYear() <= year.getFullYear()) && (year_bir2.getMonth() <= year.getMonth()) && (year_bir2.getDay() <= year.getDay())))
+
+                if(year_bir2.getFullYear() <= year.getFullYear()){
+                    if((((year_bir2.getMonth() + 1) <= (year.getMonth() + 1)) && (year_bir2.getDate() <= year.getDate())) || year_bir2.getFullYear() <= year.getFullYear()){
+
+                            this.not = true
+                            this.isActive = false
+ 
+                            if(this.rezult.rez3 == 0) this.isDate = false
+                            else this.isDate = true
+                            
+                                                    }
+                        else{
+                            this.not = false
+                            this.isActive = true
+                            return this.warning = this.w + 'не допустимая дата!'
+                        }
+                }
+                else{
+                    this.not = false
+                    this.isActive = true
+                    return this.warning = this.w + 'не допустимая дата!'
+                }
+                
+                // console.log(rez_next)
+
+                
             }
         }
 })
+
